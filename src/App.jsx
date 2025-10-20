b616b54deecc6d06c3c256867678d5aedba3108d
H4RI2000 <hari2000atl@gmail.com>
2025-10-20 18:54:32 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React,{ useState } from 'react';
+
+import Counter from './Componets/counter.jsx'
+import './App.css'
+
+function App() {
+   
+  
+  return (
+    <>
+
+     <Counter/>
+    </>
+  )
+}
+
+export default App
